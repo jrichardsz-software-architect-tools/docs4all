96ed810a10b39c3c55e9982c51d2946c582ea277
jrichardsz <jrichardsz.java@gmail.com>
2021-03-08 18:31:08 -0500

    fix hardcoded url

--- README.md
+++ README.md
@@ -2,8 +2,55 @@
 
 <img src="https://static2.cbrimages.com/wordpress/wp-content/uploads/2019/10/My-Hero-Academia-All-Might-Header.jpg" width="100%">
 
-
 Doc4all is a very lightweight web server to convert your markdown files into a usable wiki or Knowledge base for your company.
+
+# Basic Usage
+
+- Create a new npm project
+- add this package.json
+```json
+{
+  "name": "acme-docs",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "supervisor -e 'js|md' index.js"
+  },
+  "author": "",
+  "license": "MIT",
+  "dependencies": {
+    "docs4all": "software-architect-tools/docs4all",
+    "supervisor": "^0.12.0"
+  }
+}
+```
+- create this index.js
+
+```
+const Docs4All = require("docs4all");
+var docs4All = new Docs4All();
+docs4All.start({
+  "documentsLocation":__dirname+"/docs",
+  "design":{
+    "title":"ACME Docs",
+    "navHeaderFontSize":"18",
+  }
+});
+```
+
+- create a folder called "docs" and put inside any markdown file (*.md)
+- npm install
+- npm run dev
+- go to `http://localhost:2708` to view the home page
+
+# Production
+
+- npm install
+- set you desired port with `export PORT=8080`
+- npm run start
+
 
 # Inspiration
 
@@ -11,7 +58,7 @@ Doc4all is a very lightweight web server to convert your markdown files into a u
 
 # Assets
 
-https://cdnjs.cloudflare.com/ajax/libs/markdown-it/12.0.4/markdown-it.min.js
-https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css
-https://code.jquery.com/ui/1.12.1/jquery-ui.js
-https://code.jquery.com/jquery-1.12.4.js
+- https://cdnjs.cloudflare.com/ajax/libs/markdown-it/12.0.4/markdown-it.min.js
+- https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css
+- https://code.jquery.com/ui/1.12.1/jquery-ui.js
+- https://code.jquery.com/jquery-1.12.4.js
--- src/main/web/js/ApiClient.js
+++ src/main/web/js/ApiClient.js
@@ -1,8 +1,10 @@
 function ApiClient(){
 
+  var apiBaseUrl = getLocationBasePath();
+
   this.find = (resource) => {
     //add sort param
-    var url = `http://localhost:2708/api/${resource}`;
+    var url = `${apiBaseUrl}/api/${resource}`;
     return new Promise(function(resolve, reject) {
         $.ajax({
           url: url,
@@ -13,11 +15,11 @@ function ApiClient(){
           }
         });
     });
-  };  
+  };
 
   this.getTreeMenuByAudienceTargetType = () => {
     //add sort param
-    var url = `http://localhost:2708/api/menu-tree`;
+    var url = `${apiBaseUrl}/api/menu-tree`;
     url = addParam(url, "audienceTarget", "user");
     return new Promise(function(resolve, reject) {
         $.ajax({
@@ -29,11 +31,11 @@ function ApiClient(){
           }
         });
     });
-  };  
+  };
 
   this.findDocumentByAndRestrictions = (resource, query) => {
     //add sort param
-    var url = `http://localhost:2708/api/${resource}/query/and`;
+    var url = `${apiBaseUrl}/api/${resource}/query/and`;
     return new Promise(function(resolve, reject) {
         $.ajax({
           url: url,
@@ -46,23 +48,42 @@ function ApiClient(){
           }
         });
     });
-  };  
+  };
 
-  
+
   /**
-  * Add a URL parameter 
-  * @param {string} url 
+  * Add a URL parameter
+  * @param {string} url
   * @param {string} param the key to set
-  * @param {string} value 
+  * @param {string} value
   */
-  var addParam = function(url, param, value) {
+  function addParam(url, param, value) {
      param = encodeURIComponent(param);
      var a = document.createElement('a');
-     param += (value ? "=" + encodeURIComponent(value) : ""); 
+     param += (value ? "=" + encodeURIComponent(value) : "");
      a.href = url;
      a.search += (a.search ? "&" : "") + param;
      return a.href;
-  }  
+  }
+
+  function getLocationBasePath() {
+
+    if (typeof window === "undefined") {
+      console.error("ReferenceError: window is not defined. Are you in frontend javascript layer?");
+      return;
+    }
+
+    if (typeof window.location === "undefined") {
+      console.error("ReferenceError: window.location is not defined. Are you in frontend javascript layer?");
+      return;
+    }
+
+    if(window.location.port){
+      return window.location.protocol+"//"+window.location.hostname+":"+window.location.port
+    }else {
+      return window.location.protocol+"//"+window.location.hostname
+    }
+  }
 
 }
 
